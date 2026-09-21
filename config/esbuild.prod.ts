f3011c95c047640597a9feed1ba6a1f886a076c6
lineana <17570875940@163.com>
2026-09-21 10:06:43 +0800

    fix: align esbuild runner with official extension template

--- config/esbuild.prod.ts
+++ config/esbuild.prod.ts
@@ -1,7 +1,7 @@
 import process from 'node:process';
 import esbuild from 'esbuild';
 
-import common from './esbuild.common.ts';
+import common from './esbuild.common';
 
 (async () => {
 	const ctx = await esbuild.context(common);
